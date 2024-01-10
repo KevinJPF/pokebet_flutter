772ef2eb188f0ac37b0855ecf2dd4cd3a3e3f9d1
KevinJPF <kevinjuliano.pires@gmail.com>
2024-01-09 23:34:12 -0300

    feat: capture fields details

--- lib/views/capture/capture.dart
+++ lib/views/capture/capture.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:PokeBet/models/database_models.dart';
-import 'package:PokeBet/views/capture/search_pokemon.dart';
+import 'package:PokeBet/views/capture/capture_field.dart';
 import 'package:PokeBet/views/profile/player_profile.dart';
 import 'package:PokeBet/widgets/icon_container.dart';
 import 'package:PokeBet/widgets/menu_bar.dart';
@@ -18,6 +18,10 @@ class Capture extends StatefulWidget {
 }
 
 class _CaptureState extends State<Capture> {
+  String camposVerdesDescription = 'Uma vasta planície habitada principalmente por Pokémon do tipo voador e normal, enquanto tipos grama e inseto também podem ser encontrados ocasionalmente.';
+  String florestaAbetoDescription = 'Uma densa floresta de árvores de abeto onde predominam Pokémon do tipo grama, inseto e normal. Ocasionalmente, podem ser avistados Pokémon do tipo voador e terrestre.';
+  String cavernaObscuraDescription = 'Uma caverna sombria utilizada como passagem entre rotas e cidades, habitada principalmente por Pokémon do tipo pedra, terrestre e noturno. Alguns raros Pokémon elétricos e de fogo também podem ser encontrados.';
+  String lagoDePescaDescription = 'Um lago frequentemente visitado por pescadores e treinadores, sendo lar de Pokémon do tipo água. Em ocasiões mais raras, podem ser avistados Pokémon do tipo voador e normal.';
   bool canPop = false;
   bool searchingPokemon = false;
   UserPokemon? foundPokemon;
@@ -65,7 +69,13 @@ class _CaptureState extends State<Capture> {
                             onClick: () async {
                               await Navigator.of(context).push(
                                 MaterialPageRoute(
-                                  builder: (context) => SearchPokemon(),
+                                  builder: (context) => CaptureField(
+                                    fieldName: 'Campos Verdes',
+                                    fieldDescription: camposVerdesDescription,
+                                    fieldImageName: 'campos_verdes',
+                                    commonTypes: ['flying', 'normal'],
+                                    rareTypes: ['grass','bug'],
+                                  ),
                                 ),
                               );
                               setState(() {});
@@ -79,7 +89,13 @@ class _CaptureState extends State<Capture> {
                             onClick: () async {
                               await Navigator.of(context).push(
                                 MaterialPageRoute(
-                                  builder: (context) => SearchPokemon(),
+                                  builder: (context) => CaptureField(
+                                    fieldName: 'Floresta Abeto',
+                                    fieldDescription: florestaAbetoDescription,
+                                    fieldImageName: 'floresta_abeto',
+                                    commonTypes: ['grass', 'bug', 'normal'],
+                                    rareTypes: ['flying', 'ground'],
+                                  ),
                                 ),
                               );
                               setState(() {});
@@ -93,7 +109,13 @@ class _CaptureState extends State<Capture> {
                             onClick: () async {
                               await Navigator.of(context).push(
                                 MaterialPageRoute(
-                                  builder: (context) => SearchPokemon(),
+                                  builder: (context) => CaptureField(
+                                    fieldName: 'Caverna Obscura',
+                                    fieldDescription: cavernaObscuraDescription,
+                                    fieldImageName: 'caverna_obscura',
+                                    commonTypes: ['rock', 'ground','dark'],
+                                    rareTypes: ['electric', 'fire'],
+                                  ),
                                 ),
                               );
                               setState(() {});
@@ -107,7 +129,13 @@ class _CaptureState extends State<Capture> {
                             onClick: () async {
                               await Navigator.of(context).push(
                                 MaterialPageRoute(
-                                  builder: (context) => SearchPokemon(),
+                                  builder: (context) => CaptureField(
+                                    fieldName: 'Lago de Pesca',
+                                    fieldDescription: lagoDePescaDescription,
+                                    fieldImageName: 'lago_de_pesca',
+                                    commonTypes: ['water'],
+                                    rareTypes: ['flying', 'normal'],
+                                  ),
                                 ),
                               );
                               setState(() {});
---
+++ lib/views/capture/capture_field.dart
@@ -0,0 +1,166 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:PokeBet/widgets/custom_button.dart';
+import 'package:PokeBet/widgets/custom_texts.dart';
+import 'package:PokeBet/widgets/pokemon_type.dart';
+import 'package:PokeBet/widgets/top_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:PokeBet/global.dart';
+import 'package:PokeBet/widgets/background.dart';
+
+class CaptureField extends StatefulWidget {
+  final String fieldName;
+  final String fieldDescription;
+  final String fieldImageName;
+  final List<String> commonTypes;
+  final List<String> rareTypes;
+  const CaptureField({
+    super.key,
+    required this.fieldName,
+    required this.fieldDescription,
+    required this.fieldImageName,
+    required this.commonTypes,
+    required this.rareTypes,
+  });
+
+  @override
+  State<CaptureField> createState() => _CaptureFieldState();
+}
+
+class _CaptureFieldState extends State<CaptureField> {
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double safeAreaHeight = MediaQuery.of(context).padding.top +
+        MediaQuery.of(context).padding.bottom;
+
+    double availableHeight = screenHeight - safeAreaHeight;
+
+    return Scaffold(
+      backgroundColor: Global.pokebetColors.backgroundColor,
+      body: SafeArea(
+        child: Container(
+          height: availableHeight,
+          child: Column(
+            children: [
+              Expanded(
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Background(hasLogo: false),
+                    Column(
+                      children: [
+                        TopBar(showBackButton: true, pageTitle: ''),
+                      ],
+                    ),
+                    Container(
+                      alignment: Alignment.topCenter,
+                      padding: EdgeInsets.only(
+                        top: setHeight(80),
+                        bottom: setHeight(40),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  margin: EdgeInsets.symmetric(
+                                      horizontal: setWidth(32)),
+                                      decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(35),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.black.withOpacity(0.25),
+                                          spreadRadius: 0,
+                                          blurRadius: 4,
+                                          offset: Offset(0, 0),
+                                        ),
+                                      ],
+                                      ),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(35),
+                                    child: Image.asset(
+                                      'assets/imgs/${widget.fieldImageName}.jpg',
+                                      fit: BoxFit.fitWidth,
+                                      height: setHeight(150),
+                                      // width: setHeight(300),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: setHeight(16)),
+                          SimpleText(
+                            widget.fieldName,
+                            fontSize: 32,
+                          ),
+                          Container(
+                            padding:
+                                EdgeInsets.symmetric(horizontal: setWidth(32)),
+                            child: SimpleText(widget.fieldDescription),
+                          ),
+                          Spacer(),
+                          SimpleText(
+                            'Tipos mais comuns:',
+                          ),
+                          SizedBox(height: setHeight(16)),
+                          Expanded(
+                            child: Container(
+                              alignment: Alignment.center,
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: setWidth(32)),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: widget.commonTypes.map((String type) {
+                                  return Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: setWidth(4)),
+                                    child: TypeIcon(type, showTypeName: false),
+                                  );
+                                }).toList(),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: setHeight(16)),
+                          SimpleText(
+                            'Tipos mais raros:',
+                          ),
+                          SizedBox(height: setHeight(16)),
+                          Expanded(
+                            child: Container(
+                              alignment: Alignment.center,
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: setWidth(32)),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: widget.rareTypes.map((String type) {
+                                  return Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: setWidth(4)),
+                                    child: TypeIcon(type, showTypeName: false),
+                                  );
+                                }).toList(),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: setHeight(16)),
+                          CustomButton(
+                            buttonText: 'Procurar Pokemon',
+                            onPressed: () async {},
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
